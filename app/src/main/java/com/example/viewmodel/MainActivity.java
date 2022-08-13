565c60f9a0df2a5074772e6e275a95a5fca4147a
Micoder-dev <micoder.com@gmail.com>
2022-08-13 19:56:31 +0530

    A simple button click count added

--- app/src/main/java/com/example/viewmodel/MainActivity.java
+++ app/src/main/java/com/example/viewmodel/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.viewmodel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    Button mButton;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textView);
+        mButton = findViewById(R.id.clickBtn);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                mTextView.setText("You clicked "+ counter +" times");
+            }
+        });
+
     }
 }
